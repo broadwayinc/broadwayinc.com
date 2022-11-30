cf70e3d1c97122ac4da21ba94263a2e7405651da
Baksa Gimm <baksa@broadwayinc.com>
2022-11-30 22:33:42 +0900

    upload

---
+++ src/views/HomeView.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+.shell
+  .head
+    sui-flextext(max-size='1024') broadway Inc.
+    br
+    br
+    br
+    div.
+      skapi serverless cloud services#[br]
+      wato studio#[br]
+      #[br]
+      2F Yohan House, 24, Seongmisan-ro, Mapo-gu, Seoul, 03997, Republic of Korea
+</template>
+<style scoped lang="less">
+.shell {
+  max-width: 1400px;
+  margin: auto;
+  min-height: 100vh;
+  align-items: center;
+  display: flex;
+
+  .head {
+    margin: 8px;
+
+    div {
+      opacity: 0.88;
+      text-align: right;
+    }
+  }
+}
+</style>
