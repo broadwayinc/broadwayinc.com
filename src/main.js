e793cae65fa096ab2237f3429570bfab88a28108
baksagimm <baksa@broadwayinc.com>
2022-12-01 13:51:33 +0900

    added timetracker

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
 
-import './assets/base.css'
+import './assets/base.css';
 import { defineCustomElements } from 'skateui/loader';
+
+import { Skapi } from "skapi-js";
+
 defineCustomElements(window);
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router)
+app.use(router);
 
-app.mount('#app')
+app.mount('#app');
+
+export const skapi = new Skapi('ap22lazr5d2xrzhl6s77ea', '7aa4ffe1-1b06-4375-9be2-47d89da9d206');
--- src/views/TimeTracker.vue
+++ src/views/TimeTracker.vue
@@ -3,13 +3,13 @@
   .head
     h1 Time Tracker
     hr
-    br
-    br
-    br
-    br
-    div
-      h2 Login
-      form(style="display:inline-block;text-align:left;")
+
+    div(v-if="user")
+      .stamp(v-for="h in history") {{new Date(h.uploaded)}}
+
+    div(v-else)
+      h2 Login to punch In/Out
+      form(ref='loginForm' @submit.prevent="skapi.login($refs.loginForm, login_opt)")
         label(for="email") E-Mail
         sui-input#email(name='email' type='email' placeholder='your-email@broadwayinc.com' required)
         br
@@ -20,24 +20,70 @@
         br
         sui-input(type='submit')
 </template>
+<script setup>
+import { ref } from 'vue';
+import { skapi } from '../main';
 
+let user = ref(null);
+let history = ref([]);
+let lastStartKey = null;
+
+let login_opt = {
+  response: async (u) => {
+    user.value = u;
+    await skapi.postRecord(null, {
+      table: 'timetrack'
+    });
+    let rec = await skapi.getRecords({
+      table: 'timetrack',
+      reference: skapi.user.user_id
+    }, {
+      ascending: false,
+      limit: 60,
+      startKey: lastStartKey
+    });
+    lastStartKey = rec.startKey;
+    history.value = history.value.concat(rec.list);
+  }
+}
+
+</script>
 <style scoped lang="less">
 .shell {
   max-width: 1024px;
-  margin:auto;
+  margin: auto;
 
   .head {
     margin: 8px;
-    label {
-      display: block;
-      font-weight: bold;
-      margin-bottom: 4px;
-    }
-    sui-input {
-      width: 20em;
-    }
-    &>div{
+
+    &>div {
       text-align: center;
+
+      .stamp {
+        display: block;
+        background: rgba(0 0 0 / 8%);
+        padding: 0.5em;
+        border-radius: 8px;
+        font-weight: bold;
+        margin-bottom: 4px;
+        text-align: left;
+        font-family: monospace;
+      }
+
+      form {
+        display: inline-block;
+        text-align: left;
+
+        label {
+          display: block;
+          font-weight: bold;
+          margin-bottom: 4px;
+        }
+
+        sui-input {
+          width: 20em;
+        }
+      }
     }
   }
 }
