40c904999bab1e0addda0f89bf0e5e289406bab7
dev <dev@broadwayinc.com>
2022-12-19 20:22:49 +0000

    update time punch

--- src/views/TimePunch.vue
+++ src/views/TimePunch.vue
@@ -4,38 +4,58 @@
   br
   .head
     h1(style="margin:0;width:calc(100% - 128px);display:inline-block;vertical-align:middle;") {{user ? 'Hello ' + (user.name || user.email) : 'Time Punch'}}
+
     router-link(to="profile")
       sui-button(v-if="user" style="width: 128px;background-color:transparent;color:black;border:2px solid rgba(0 0 0 / 50%);vertical-align:middle;") Edit Profile
+
     hr
+
     div(v-if="user")
-      .stamp(v-if="!history_computed.length") ... History loading
-      template(v-for="h, idx in history_computed")
-        .stamp(v-if="idx < history_computed.length - 2 || lastStartKey === 'end'" :style="{'color': h.opt === 'in' ? 'green' : 'red'}") {{h.punch.toLocaleString()}} [{{h.opt === 'in' ? 'in' : 'out:&nbsp;'}}{{h.opt === 'out' ? h.duration : ''}}]
+      // history box
+      
+      template(v-if="!history.length")
+        // loader
+        .stamp(v-if="didCall") No history
+        .stamp(v-else) ... History loading
+      
+      template(v-else)
+        // history
+        .stamp(v-for="h, idx in history" :style="{'color': h.opt === 'in' ? 'green' : 'red'}") {{h.punch.toLocaleString()}} [{{h.opt === 'in' ? 'in' : 'out:&nbsp;'}}{{h.opt === 'out' ? h.duration : ''}}]
+      
       br
+      
       sui-button(v-if="lastStartKey && lastStartKey !== 'end'" @click='getRecords') Load More
 
     div(v-else)
+      // login box
+
       h2 Login to punch In/Out
+      
       form(ref='loginForm' @submit.prevent="skapi.login($refs.loginForm, login_opt)")
         label(for="email") E-Mail
         sui-input#email(name='email' type='email' placeholder='your-email@broadwayinc.com' required)
+        
         br
         br
+        
         label(for="pw") Password
         sui-input#pw(name='password' type='password' placeholder='Password' required)
+        
         br
         br
+        
         sui-input(type='submit')
         
 </template>
 <script setup>
-import { computed, onUnmounted, ref } from 'vue';
+import { onUnmounted, ref } from 'vue';
 import { skapi, user } from '../main';
 
 let history = ref([]);
 let lastStartKey = ref(null);
-let punchOut = null;
-let punchHold = null;
+let lastPunch = null;
+let startPunch = null;
+let didCall = ref(false);
 
 onUnmounted(() => {
   lastStartKey.value = null;
@@ -50,66 +70,31 @@ function msToHMS(ms) {
   return { hours, minutes, seconds, str: `${hours}h ${minutes}m ${seconds}s` };
 }
 
-let history_computed = computed({
-  get() {
-    let arr = [];
-    if (history.value.length) {
-      if (lastStartKey.value === 'end' && punchHold) {
-        // end of list
-        arr.push({ opt: 'out', punch: punchOut, duration: msToHMS(punchOut - punchHold).str });
-        arr.push({ opt: 'in', punch: punchHold });
-      }
-    }
-    else if (punchOut) {
-      if (punchHold) {
-        arr.push({ opt: 'out', punch: punchOut, duration: msToHMS(punchOut - punchHold).str });
-        arr.push({ opt: 'in', punch: punchHold });
-      }
-      else {
-        arr.push({ opt: 'in', punch: punchOut });
-      }
-    }
+function resolveLeftovers(punchTime) {
+  let arr = history.value;
 
-    // return with additional concat
-    return history.value.concat(arr);
-  }
-});
-
-function sortHistory(list) {
-  let arr = [];
-
-  for (let h of list) {
-    if(h.ip !== '211.217.251.144') {
-      // wrong record ip
-      continue;
-    }
-
-    let punchTime = new Date(h.uploaded);
-
-    if (!punchOut) {
-      punchOut = punchTime;
-      continue;
-    }
-
-    let diff = msToHMS(punchOut - punchTime);
-    if (diff.hours <= 11) {
-      // if within range (12 hour range)
-      punchHold = punchTime;
-    }
-    else {
-      // if out of range, show punch in/out
-      if (punchHold) {
-        arr.push({ opt: 'out', punch: punchOut, duration: msToHMS(punchOut - punchHold).str });
-        arr.push({ opt: 'in', punch: punchHold });
-        punchOut = punchTime;
-      }
-    }
+  if (startPunch) {
+    // use lastPunch(out) / startPunch(in)
+    arr.push({ opt: 'out', punch: lastPunch, duration: msToHMS(lastPunch - startPunch).str });
+    arr.push({ opt: 'in', punch: startPunch });
   }
 
-  return arr;
+  else if (lastPunch) {
+    // use lastPunch(in)
+    arr.push({ opt: 'in', punch: lastPunch });
+  }
+
+  if (punchTime) {
+    lastPunch = punchTime;
+  }
 }
 
 async function getRecords() {
+  if (lastStartKey.value === 'end') {
+    // no more fetching
+    return;
+  }
+
   let rec = await skapi.getRecords({
     table: 'timestamp',
     reference: user.value.user_id,
@@ -120,8 +105,45 @@ async function getRecords() {
     refresh: !lastStartKey.value
   });
 
+  didCall.value = true;
+
+  let list = rec.list;
+  for (let h of list) {
+    // h = punch record
+
+    if (h.ip !== '211.217.251.144') {
+      // wrong record ip
+      continue;
+    }
+
+    let punchTime = new Date(h.uploaded);
+
+    // history = [ out, ... within 12 hour , in ]
+    // history = [ in, ... passed 12 hour , out ]
+
+    if (!lastPunch) {
+      lastPunch = punchTime;
+      continue;
+    }
+
+    let diff = msToHMS(lastPunch - punchTime);
+
+    if (diff.hours <= 11) {
+      // within 12 hour
+      startPunch = punchTime;
+    }
+    else {
+      // out of range
+      resolveLeftovers(punchTime);
+    }
+  }
+
+  if (rec.startKey === 'end') {
+    // end of list, resolve leftovers
+    resolveLeftovers();
+  }
+
   lastStartKey.value = rec.startKey;
-  history.value = history.value.concat(sortHistory(rec.list));
 }
 
 let login_opt = {
@@ -138,6 +160,7 @@ let login_opt = {
 };
 
 if (user.value) {
+  // already logged in
   getRecords();
 }
 
