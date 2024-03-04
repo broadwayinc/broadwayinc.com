18c983436f53018fddec443c17364c908321d7c4
dev <dev@broadwayinc.com>
2024-03-04 08:24:26 +0000

    update

--- admin/tickets.js
+++ admin/tickets.js
@@ -1,14 +1,51 @@
+// random string generator
+function randomString(length) {
+    let result = '';
+    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+function changeType(event, rand) {
+    let input = document.getElementById(rand);
+    let newInput = document.createElement('input');
+    newInput.required = true;
+    newInput.id = rand;
+    newInput.type = event.target.value;
+    newInput.name = input.name;
+    newInput.required = true;
+    input.replaceWith(newInput);
+}
+
 function addMore(event, key, type) {
     // element before the button
     let element = event.target.previousElementSibling;
     let keyInp = /*html*/ `
         <td>key</td>
-        <td><input type="text" name="${key}[key]" /></td>
+        <td><input type="text" name="${key}[key]" required/></td>
     `
     let valueInp = /*html*/ `
         <td>value</td>
-        <td><input type="text" name="${key}[value]" /></td>
+        <td><input type="text" name="${key}[value]" required/></td>
     `
+    
+    let typeInpGen = ()=>{
+        let rand = randomString(5);
+        return /*html*/ `
+            <td>value</td>
+            <td style="display:flex;align-items:center;justify-content:space-between;width:100%;box-sizing: border-box;"><input type="text" id="${rand}" name="${key}[value]" required/>
+            <select style="margin-left:4px;" value="text" onchange="changeType(event, '${rand}')">
+                <option value="text">String</option>
+                <option value="number">Number</option>
+                <option value="checkbox">Boolean</option>
+            </select>
+        </td>
+        `
+    }
+
     let opInp = /*html*/ `
         <td>operator</td>
         <td>
@@ -32,20 +69,24 @@ function addMore(event, key, type) {
     `
 
     let add = []
-    if (type === true) {
+    if (type === 'match') {
         add = [keyInp, valueInp, opInp, sv, im];
     }
     else if (type === 'key-value') {
         add = [keyInp, valueInp];
     }
+    else if (type === 'key-value-type') {
+        add = [keyInp, typeInpGen()];
+    }
+    else if (type === 'value') {
+        add = [valueInp, opInp];
+    }
     else {
         add = [keyInp, valueInp, opInp];
     }
-    let table = document.createElement('table');
     for (let d of add) {
         let tr = document.createElement('tr');
         tr.innerHTML = d;
-        table.appendChild(tr);
+        event.target.previousElementSibling.appendChild(tr);
     }
-    element.parentNode.insertBefore(table, event.target);
 }
